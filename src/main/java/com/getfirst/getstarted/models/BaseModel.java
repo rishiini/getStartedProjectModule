34015ba6c3cc11e10ab9584b842cb6cfe3662cdc
Rishi Bawankule <rishibawankule@gmail.com>
2024-04-26 18:01:25 +0530

    Added Getter and Setter

--- src/main/java/com/getfirst/getstarted/models/BaseModel.java
+++ src/main/java/com/getfirst/getstarted/models/BaseModel.java
@@ -1,7 +1,12 @@
 package com.getfirst.getstarted.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class BaseModel {
     private Long id;
     private Date createdAt;
